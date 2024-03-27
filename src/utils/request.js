bd9b8c0013bc57667b060d10c92ddcb9cc8bb400
鲸鱼娘西丝特official <2903039102@qq.com>
2024-03-27 15:04:50 +0800

    没什么

---
+++ src/utils/request.js
@@ -0,0 +1,79 @@
+//定制请求的实例
+
+//导入axios  npm install axios
+import axios from 'axios';
+
+//导入elementplusUi
+import { ElMessage } from 'element-plus';
+
+//定义一个变量,记录公共的前缀  ,  baseURL
+// const baseURL = 'http://localhost:8080';
+const baseURL = '/api';
+const instance = axios.create({baseURL})
+
+import { useTokenStore } from '@/stores/token.js';
+
+//添加请求拦截器
+instance.interceptors.request.use(
+    (config)=>{
+        //请求前的回调
+        //添加token
+        const tokenStore = useTokenStore();
+        // 判断有没有token
+        if(tokenStore.token){
+            config.headers.Authorization = tokenStore.token
+        }
+        return config
+    },
+    (err)=>{
+        //请求错误的回调
+        Promise.reject(err)
+    }
+)
+
+
+
+
+
+
+
+
+
+//导入路由器
+import  router  from '@/router';
+
+
+//添加响应拦截器
+instance.interceptors.response.use(
+    result=>{
+
+        //判断是否成功
+        if(result.data.code === 0){
+            return result.data;
+        }else{
+            //处理响应失败的提示
+        ElMessage.error(result.data.msg ? result.data.msg : '服务异常')
+        
+        //异步操作的状态转换为失败
+        return Promise.reject(result.data)
+        }
+
+        
+    },
+    err=>{
+        //判断响应状态码，401未登录，跳转登录登录页面
+        if(err.response.states === 401){
+            ElMessage.error('请先登录')
+            router.push('/login')
+        }else{
+            ElMessage.error('服务异常')
+        }
+        
+        alert('服务异常');
+
+        //异步的状态转化成失败的状态
+        return Promise.reject(err);
+    }
+)
+
+export default instance;
