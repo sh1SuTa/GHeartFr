a81448fa81b609230ab498c8477f9bf3c71b1559
鲸鱼娘西丝特official <2903039102@qq.com>
2024-09-19 21:30:22 +0800

    小改

--- src/components/TransgenderFlag.vue
+++ src/components/TransgenderFlag.vue
@@ -1,4 +1,4 @@
-<!-- src/components/TransgenderFlag.vue -->
+<!-- '@/components/TransgenderFlag.vue' -->
 <template>
     <svg class="transgender-flag" viewBox="0 0 100 60" xmlns="http://www.w3.org/2000/svg">
       <rect width="100" height="12" fill="#55CDFC"/> <!-- 蓝色 -->
@@ -10,12 +10,12 @@
   </template>
   
   <script setup>
-    // 可以在这里添加 JavaScript 逻辑（如果有需要）
+
   </script>
   
   <style scoped>
   .transgender-flag {
-    width: 27px; /* 调整为你需要的大小 */
+    width: 26px; /* 调整为你需要的大小 */
     height: 20px;
     display: block;
   }
